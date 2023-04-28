e5b47a8fb0b1584e139b37baeb28d96ecd7f8800
Anatoliy Popov <anatoliy@deovr.com>
2023-04-28 19:41:35 +0300

    Add player rotation based on mouse position

--- top_down_shooter.py
+++ top_down_shooter.py
@@ -16,9 +16,20 @@ background = pygame.transform.scale(pygame.image.load('background/background.png
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.transform.rotozoom(pygame.image.load("player/0.png").convert_alpha(), 0, PLAYER_SIZE)
         self.pos = pygame.math.Vector2(PLAYER_START_X, PLAYER_START_Y)
+        self.image = pygame.transform.rotozoom(pygame.image.load("player/0.png").convert_alpha(), 0, PLAYER_SIZE)
+        self.base_player_image = self.image
+        self.hit_box_rect = self.base_player_image.get_rect(center = self.pos)
+        self.rect = self.hit_box_rect.copy()
         self.speed = PLAYER_SPEED
+    
+    def player_rotation(self):
+        self.mouse_coords = pygame.mouse.get_pos()
+        self.x_change_mouse_player = (self.mouse_coords[0] - self.hit_box_rect.centerx)
+        self.y_change_mouse_player = (self.mouse_coords[1] - self.hit_box_rect.centery)
+        self.angle = math.degrees(math.atan2(self.y_change_mouse_player, self.x_change_mouse_player))
+        self.image = pygame.transform.rotate(self.base_player_image, -self.angle)
+        self.rect = self.image.get_rect(center = self.hit_box_rect.center)
     
     def user_input(self):
         self.velocity_x = 0
@@ -40,10 +51,13 @@ class Player(pygame.sprite.Sprite):
     
     def move(self):
         self.pos += pygame.math.Vector2(self.velocity_x, self.velocity_y)
+        self.hit_box_rect.center = self.pos
+        self.rect.center = self.hit_box_rect.center
     
     def update(self):
         self.user_input()
         self.move()
+        self.player_rotation()
         
 player = Player()
 
@@ -58,8 +72,11 @@ while True:
             exit()
             
     screen.blit(background, (0, 0))
-    screen.blit(player.image, (player.pos))
+    screen.blit(player.image, player.rect)
     player.update()
+    
+    pygame.draw.rect(screen, "red", player.hit_box_rect, width = 2)
+    pygame.draw.rect(screen, "yellow", player.rect, width = 2)
     
     pygame.display.update()
     clock.tick(FPS)
